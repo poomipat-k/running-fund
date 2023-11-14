19a1cd6f99c2e29713e274b449d5a1c507247b3e
poomipat-k <k.poomipat@gmail.com>
2023-11-14 11:03:15 +0700

    review add unique combination to review_criteria table

--- migrations/20231029233506_create_review_criteria_table.sql
+++ migrations/20231029233506_create_review_criteria_table.sql
@@ -4,10 +4,12 @@ CREATE TABLE review_criteria (
   criteria_version  SMALLINT NOT NULL,
   group_number SMALLINT,
   in_group_number SMALLINT,
-  order_number SMALLINT,
+  order_number SMALLINT NOT NULL,
   question_text VARCHAR(512),
   display_text VARCHAR(512)
 );
+
+ALTER TABLE review_criteria ADD CONSTRAINT uq_criterria_version_order_number UNIQUE(criteria_version, order_number);
 
 -- +goose Down
 DROP TABLE review_criteria;
