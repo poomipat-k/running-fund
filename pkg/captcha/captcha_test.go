c277b164f74258194a9e5b49908c55a7c414c839
poomipat-k <k.poomipat@gmail.com>
2024-01-12 18:21:11 +0700

    auth add TestCheckCaptcha

--- pkg/captcha/captcha_test.go
+++ pkg/captcha/captcha_test.go
@@ -80,7 +80,94 @@ func TestGenerateCaptcha(t *testing.T) {
 	if got.Puzzle64 != wantBase64Puzzle {
 		t.Errorf("base64 got %s, want %s", got.Background64, wantBase64Puzzle)
 	}
+}
 
+func TestCheckCaptcha(t *testing.T) {
+	store := &MockCaptchaStore{}
+
+	// Payload validation cases
+	tests := []struct {
+		name          string
+		captchaId     string
+		captchaValue  float64
+		expectedError error
+	}{
+		{
+			name:          "should error when captchaId is missing",
+			captchaId:     "",
+			captchaValue:  40,
+			expectedError: &captcha.CaptchaIdRequiredError{},
+		},
+		{
+			name:          "should error when captchaValue is missing",
+			captchaId:     "abc",
+			expectedError: &captcha.CaptchaValueRequiredError{},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := captcha.CheckCaptcha(tt.captchaId, tt.captchaValue, store)
+			if tt.expectedError != nil {
+				assertErrorMessage(t, err.Error(), tt.expectedError.Error())
+			}
+		})
+	}
+
+	// Mock data
+	data := make(map[string]float64)
+	store = &MockCaptchaStore{
+		Store: data,
+		GenerateCaptchaFunc: func() (captcha.Captcha, error) {
+			captchaId := "RhEzlSh46ClI"
+			captchaValue := 47.2
+			data[captchaId] = captchaValue
+			return captcha.Captcha{
+				CaptchaId:    captchaId,
+				Background64: "abc",
+				Puzzle64:     "def",
+			}, nil
+		},
+		GetFunc: func(captchaId string) (float64, bool) {
+			v, ok := data[captchaId]
+			return v, ok
+		},
+	}
+
+	t.Run("should error when captchaId is not found", func(t *testing.T) {
+		_, _ = store.GenerateCaptchaFunc()
+		id := "RhEzlSh_TEST"
+		value := 46.5
+		_, err := captcha.CheckCaptcha(id, value, store)
+		want := &captcha.CaptchaNotFoundError{}
+		assertErrorMessage(t, err.Error(), want.Error())
+	})
+
+	t.Run("should error when captchaValue is out of accept limit", func(t *testing.T) {
+		_, _ = store.GenerateCaptchaFunc()
+		id := "RhEzlSh46ClI"
+		value := 12.5
+		_, err := captcha.CheckCaptcha(id, value, store)
+		want := &captcha.CaptchaValueNotValidError{}
+		assertErrorMessage(t, err.Error(), want.Error())
+	})
+
+	t.Run("should be ok", func(t *testing.T) {
+		_, _ = store.GenerateCaptchaFunc()
+		id := "RhEzlSh46ClI"
+		value := 42.5
+		_, err := captcha.CheckCaptcha(id, value, store)
+		if err != nil {
+			t.Errorf("should be ok but got error: %v", err)
+		}
+	})
+}
+
+func assertErrorMessage(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("did not get correct error, got %v, want %v", got, want)
+	}
 }
 
 func assertStatus(t testing.TB, got, want int) {
