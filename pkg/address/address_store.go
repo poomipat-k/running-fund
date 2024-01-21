b3e949b2fcca329286b1bdd5264a9c6f08eb4f90
poomipat-k <k.poomipat@gmail.com>
2024-01-21 23:28:00 +0700

    applicant get districtsByProvince endpoint

--- pkg/address/address_store.go
+++ pkg/address/address_store.go
@@ -2,20 +2,39 @@ package address
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
+
+	"github.com/patrickmn/go-cache"
 )
+
+const provinceCachePrefix = "province"
+const distByProv = "distByProv"
 
 type store struct {
 	db *sql.DB
+	c  *cache.Cache
 }
 
-func NewStore(db *sql.DB) *store {
+func NewStore(db *sql.DB, c *cache.Cache) *store {
 	return &store{
 		db: db,
+		c:  c,
 	}
 }
 
 func (s *store) GetProvinces() ([]Province, error) {
+	// check cache first
+	rawCachedData, found := s.c.Get(fmt.Sprintf("%sall", provinceCachePrefix))
+	if found {
+		log.Println("===PROVINCES found in cache")
+		cachedData, ok := rawCachedData.([]Province)
+		if ok {
+			log.Println("===cached Used")
+			return cachedData, nil
+		}
+	}
+	// Fetch data from the db
 	rows, err := s.db.Query(getProvincesSQL)
 	if err != nil {
 		return nil, err
@@ -36,31 +55,34 @@ func (s *store) GetProvinces() ([]Province, error) {
 	if err != nil {
 		return nil, err
 	}
-	log.Println("===store data", data)
+	s.c.Set(fmt.Sprintf("%s__all", provinceCachePrefix), data, cache.NoExpiration)
 	return data, nil
 }
 
-// func (s *store) GetDistricts() ([]District, error) {
-// 	rows, err := s.db.Query(getDistrictsSQL)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	defer rows.Close()
+func (s *store) GetDistrictsByProvince(provinceId int) ([]District, error) {
 
-// 	var data []Province
-// 	for rows.Next() {
-// 		var row Province
-// 		err := rows.Scan(&row.Id, &row.Name)
-// 		if err != nil {
-// 			return nil, err
-// 		}
-// 		data = append(data, row)
-// 	}
+	rows, err := s.db.Query(getDistrictsSQL, provinceId)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
 
-// 	err = rows.Err()
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	log.Println("===store data", data)
-// 	return data, nil
-// }
+	var data []District
+	for rows.Next() {
+		var row District
+		err := rows.Scan(&row.Id, &row.Name)
+		if err != nil {
+			return nil, err
+		}
+		data = append(data, row)
+	}
+
+	err = rows.Err()
+	if err != nil {
+		return nil, err
+	}
+	log.Println("===dist data")
+	log.Println(data)
+	s.c.Set(fmt.Sprintf("%s__%d", distByProv, provinceId), data, cache.NoExpiration)
+	return data, nil
+}
