358d0c6efcef9243392938340aa6fa6398c10319
poomipat-k <k.poomipat@gmail.com>
2024-02-21 19:38:39 +0700

    applicant init 4.1

--- pkg/projects/validation_experience.go
+++ pkg/projects/validation_experience.go
@@ -4,5 +4,8 @@ func validateExperience(payload AddProjectRequest) error {
 	if payload.Experience.ThisSeries.FirstTime == nil {
 		return &ThisSeriesFirstTimeRequiredError{}
 	}
+	if payload.Experience.ThisSeries.History.OrdinalNumber < 1 {
+		return &HistoryOrdinalNumberInvalidError{}
+	}
 	return nil
 }
