917213d3a09455faaf8069219d29559a75418595
poomipat-k <k.poomipat@gmail.com>
2024-03-07 20:31:51 +0700

    applicant add presign endpoint for UI to use with path per permission

--- pkg/s3-service/handler.go
+++ pkg/s3-service/handler.go
@@ -1,8 +1,8 @@
 package s3Service
 
 import (
+	"errors"
 	"fmt"
-	"log"
 	"log/slog"
 	"net/http"
 	"os"
@@ -28,7 +28,6 @@ func (h *S3Handler) GeneratePresignedUrl(w http.ResponseWriter, r *http.Request)
 		utils.ErrorJSON(w, err, "userId", http.StatusForbidden)
 		return
 	}
-	log.Println("==userId", userId)
 
 	// payload.path start with project_code without prefix /
 	var payload GetPresignedPayload
@@ -37,12 +36,25 @@ func (h *S3Handler) GeneratePresignedUrl(w http.ResponseWriter, r *http.Request)
 		utils.ErrorJSON(w, err, "payload", http.StatusBadRequest)
 	}
 
+	userRole := utils.GetUserRoleFromRequestHeader(r)
+	if userRole == "" {
+		msg := "userRole is empty"
+		err = errors.New(msg)
+		slog.Error(err.Error())
+		utils.ErrorJSON(w, err, "userRole", http.StatusForbidden)
+		return
+	}
+
+	var objectKey string
 	bucketName := os.Getenv("AWS_S3_STORE_BUCKET_NAME")
-	objectKey := fmt.Sprintf("applicant/user_%d/%s", userId, payload.Path)
-	presignResult, err := h.presigner.GetObject(bucketName, objectKey, 3600)
+	if userRole == "applicant" {
+		objectKey = fmt.Sprintf("applicant/user_%d/%s", userId, payload.Path)
+	} else {
+		objectKey = fmt.Sprintf("applicant/user_%d/%s", payload.ProjectCreatedByUserId, payload.Path)
+	}
+	presignedResult, err := h.presigner.GetObject(bucketName, objectKey, 3600)
 	if err != nil {
 		utils.ErrorJSON(w, err, "presign")
 	}
-	log.Println("==presignResult", presignResult)
-	utils.WriteJSON(w, http.StatusOK, presignResult)
+	utils.WriteJSON(w, http.StatusOK, presignedResult)
 }
